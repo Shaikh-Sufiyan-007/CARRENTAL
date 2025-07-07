79fe1869658adc825aa01396b6368bb8988b3d80
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-07-07 18:39:45 +0530

    1 - creating Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
   return (
-    <div className='text-rose-900'>App</div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+    </>
   )
 }
 
