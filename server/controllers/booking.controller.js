343f9bb388b46603f87ab93a8803eecda034b496
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-07-16 20:36:54 +0530

    2 - building createBooking Controller function

--- server/controllers/booking.controller.js
+++ server/controllers/booking.controller.js
@@ -30,9 +30,36 @@ export const checkAvailabilityOfCar = async(req, res) => {
         availableCars = availableCars.filter(car => car.isAvaliable === true);
 
         res.status(200).json({success: true, message: "Availability fetched successfully", availableCars});
-        
+
     } catch (error) {
         console.log("Error in checkAvailabilityOfCar", error.message);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const createBooking = async(req, res) => {
+    try {
+        const {_id} = req.user;
+        const {car, pickupDate, returnDate} = req.body;
+
+        const isAvailable = await checkAvalability(car, pickupDate, returnDate);
+        if(!isAvailable) {
+            return res.status(400).json({success: false, message: "Car is not available for the given date"});
+        }
+
+        const carData = await Car.findById(car);
+
+        const picked = new Date(pickupDate);
+        const returned = new Date(returnDate);
+
+        const noOfDays = Math.ceil((returned - picked) / (1000 * 60 * 60 * 24));
+        const price = carData.pricePerDay * noOfDays;
+
+        await Booking.create({car, owner: carData.owner, user: _id, pickupDate, returnDate, price})
+
+        res.status(200).json({success: true, message: "Booking created successfully"});
+    } catch (error) {
+        console.log("Error in createBooking", error.message);
         return res.status(500).json({ success: false, message: error.message });
     }
 }
